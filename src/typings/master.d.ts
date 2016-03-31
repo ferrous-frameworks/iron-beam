896809c141e1bd69e3e9a6569bf7cb785d85463f
Tyson Hester <ironman9967@gmail.com>
2016-03-31 18:40:38 +0000

    added oss files and switched to typings from tsd

--- src/typings/master.d.ts
+++ src/typings/master.d.ts
@@ -1,4 +1,4 @@
 
-///<reference path='./tsd/tsd.d.ts' />
+///<reference path='../../typings/main.d.ts' />
 
 ///<reference path='../../node_modules/iron-tree/dist/iron-tree.d.ts' />
--- src/typings/tsd/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="async/async.d.ts" />
-/// <reference path="chai/chai.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="node/node.d.ts" />
