8a2fc310c31c922b4bf2760b3d926e552bf6dd22
Tyson Hester <ironman9967@gmail.com>
2016-05-09 17:52:30 +0000

    added typings tag in package.json

--- dist/event-emitter/iron-beam.js
+++ dist/event-emitter/iron-beam.js
@@ -1,4 +1,5 @@
 //<reference path='../typings/master.d.ts' />
+"use strict";
 var _ = require('lodash');
 var async = require('async');
 var IronTree = require('iron-tree');
@@ -272,6 +273,6 @@ var EventEmitter = (function () {
         return emitter.listeners(eventName).length;
     };
     return EventEmitter;
-})();
+}());
 exports.EventEmitter = EventEmitter;
 //# sourceMappingURL=iron-beam.js.map
